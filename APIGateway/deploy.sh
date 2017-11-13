c0d6a4c49768a6b72ffb0df640b1b0993164c5f8
rummani <33236365+rummani@users.noreply.github.com>
2017-11-13 11:43:55 -0700

    Update deploy.sh

--- APIGateway/deploy.sh
+++ APIGateway/deploy.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
 pwd
-source ./setup/setenv.sh
+mvn apigee-enterprise:deploy -P test -Dusername=rummani@prokarma.com -Dpassword=Nov@2017
 
-echo "Enter your password for the Apigee Enterprise organization $org, followed by [ENTER]:"
-
-read -s password
-
-echo Deploying $api_name to $env on $url using $username and $org and $pwd
-pwd
-./tools/deploy.py -n $api_name -u $username:$pwd -o $org -h $url -e $env -p / -d ./$api_name
-
-echo "If 'State: deployed', then your API Proxy is ready to be invoked."
-
-echo "Run 'invoke.sh'"
